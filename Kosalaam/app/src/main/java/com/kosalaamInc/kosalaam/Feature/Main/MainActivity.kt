6fbf39bfc727d2d25d0b92f93741879bb5b38b77
rhkrwngud445 <rhkrwngud445@gmail.com>
2021-07-14 15:29:02 +0900

    test : MainActivity test

--- Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Main/MainActivity.kt
+++ Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Main/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kosalaamInc.kosalaam.R
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
