cc37bd2babbe798dbc414740bb3fc5328f898742
rhkrwngud445 <rhkrwngud445@gmail.com>
2021-07-15 15:35:56 +0900

    feat : add bnv fragment

--- Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Main/CompassFragment/CompassFragment.kt
+++ Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Main/CompassFragment/CompassFragment.kt
@@ -1,2 +1,17 @@
 package com.kosalaamInc.kosalaam.Feature.Main.CompassFragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CompassFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
