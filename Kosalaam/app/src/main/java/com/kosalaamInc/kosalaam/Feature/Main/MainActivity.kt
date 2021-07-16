848a755198584b9411a90db348c1aa1560464aaa
rhkrwngud445 <rhkrwngud445@gmail.com>
2021-07-16 18:10:43 +0900

    feat : add MainFragment.kt prayerRoomButton Logic

--- Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Main/MainActivity.kt
+++ Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Main/MainActivity.kt
@@ -2,32 +2,42 @@ package com.kosalaamInc.kosalaam.Feature.Main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kosalaamInc.kosalaam.Feature.Main.MyPageFragment.MyPageFragment
 import com.kosalaamInc.kosalaam.R
 import com.kosalaamInc.kosalaam.databinding.ActivityMainBinding
 
 // bottom navigation view
 class MainActivity : AppCompatActivity() {
-
     private lateinit var mBinding : ActivityMainBinding
-
+    private lateinit var navController : NavController
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        initBottomNav()
+        initNavigationUI()
+
+
 
     }
 
-    private fun initBottomNav(){
+    private fun initNavigationUI(){
         // navigation host
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
         // navigation controller
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         NavigationUI.setupWithNavController(mBinding.bnvMain,navController)
 
     }
+//    fun mainBtEvent(){
+//        var navHostFragment = supportFragmentManager.beginTransaction().replace(R.id.fcv_main,MyPageFragment()).commit()
+//        NavigationUI.setupWithNavController(mBinding.bnvMain,navController)
+//    }
+    //mainFragment 이벤트를 여기로 전해줄수 있도록
 }
--- Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Main/MainFragment/MainFragment.kt
+++ Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Main/MainFragment/MainFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.kosalaamInc.kosalaam.Feature.Main.MainActivity
+import com.kosalaamInc.kosalaam.R
 import com.kosalaamInc.kosalaam.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(){
@@ -15,23 +18,30 @@ class MainFragment : Fragment(){
     }
 
     private var mBinding : FragmentMainBinding? = null
-
+    val MainActivity = com.kosalaamInc.kosalaam.Feature.Main.MainActivity()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentMainBinding.inflate(inflater,container,false)
-        Log.d(Tag,"OnCreateView")
+
 
         mBinding = binding
 
-        return mBinding?.root
+        initPrayerBt()
 
+        return mBinding?.root
     }
 
     override fun onDestroyView() {
         mBinding= null
         super.onDestroyView()
+    }
+
+    private fun initPrayerBt(){
+        mBinding!!.btMainTest.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainFragment_to_prayerRoomFragment)
+        }
     }
 }
