d7863f5cb259de8a83452adb33cceed3661907f1
rhkrwngud445 <rhkrwngud445@gmail.com>
2021-07-23 11:32:43 +0900

    feat : add loginActivity resource file

--- Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Login/LoginActivity.kt
+++ Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/Feature/Login/LoginActivity.kt
@@ -2,9 +2,12 @@ package com.kosalaamInc.kosalaam.Feature.Login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kosalaamInc.kosalaam.R
 
 class LoginActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
     }
 }
