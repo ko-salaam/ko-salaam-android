c03705bd8539fe8c7e35b9126b6264e906ab9d30
rhkrwngud445 <rhkrwngud445@gmail.com>
2021-09-10 23:18:52 +0900

    feat : add search ui

--- Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/feature/main/prayerRoomFragment/PrayerRoomFragment.kt
+++ Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/feature/main/prayerRoomFragment/PrayerRoomFragment.kt
@@ -6,17 +6,25 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.RelativeLayout
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.kosalaamInc.kosalaam.R
 import com.kosalaamInc.kosalaam.databinding.FragmentSearchprayerroomBinding
+import com.kosalaamInc.kosalaam.feature.main.MainActivity
 import kotlinx.coroutines.CoroutineScope
 import net.daum.android.map.MapViewEventListener
 import net.daum.mf.map.api.MapPOIItem
@@ -29,7 +37,16 @@ class PrayerRoomFragment : Fragment(),MapView.MapViewEventListener{
     private lateinit var viewModel : PrayerRoomViewModel
     lateinit var mapViewContainer : RelativeLayout
     private val TAG = "PrayerRoomFragment"
+
     val permission = android.Manifest.permission.ACCESS_FINE_LOCATION
+    companion object{
+        var margin : Float = 0.0f
+    }
+
+    private var displayStatus : Int = 1
+    // 1 : default
+    // 2 : doing search
+    // 3 : end search
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,8 +54,10 @@ class PrayerRoomFragment : Fragment(),MapView.MapViewEventListener{
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_searchprayerroom,container,false)
+        setMarginBottom()
         initMapVIew()
         checkPermission()
+        initBottomSheetView()
         return binding!!.root
     }
 
@@ -56,21 +75,117 @@ class PrayerRoomFragment : Fragment(),MapView.MapViewEventListener{
 
     private fun initObserve(){
         with(viewModel){
+            focus_et.observe(this@PrayerRoomFragment, Observer {
+                it.getContentIfNotHandled()?.let {
+                    if(it==true){
+                        changeDisplay(2)
+                    }
+
+                    else{
+                        Log.d("SearchPrayerRoomCheck","false")
+                    }
+                }
+            })
+
+            back_bt.observe(this@PrayerRoomFragment, Observer {
+                it.getContentIfNotHandled()?.let {
+                    hideKeyboard()
+                    changeDisplay(1)
+                }
+
+
+            })
+            searchKey_bt.observe(this@PrayerRoomFragment, Observer {
+               it.getContentIfNotHandled()?.let {
+                   Log.d("searchKeyActivity","true")
+                   changeDisplay(3)
+                   hideKeyboard()
+
+               }
+            })
         }
+    }
+
+    private fun changeDisplay(status : Int){
+        if(status==1){
+            displayStatus=1
+            setMarginBottom()
+            binding!!.tvSearchRecent.visibility=View.INVISIBLE
+            binding!!.ivSearchDefault.visibility=View.INVISIBLE
+            binding!!.tvSearchDefault.visibility=View.INVISIBLE
+            binding!!.clSearchWhite.visibility= View.INVISIBLE
+            binding!!.searchMapview.visibility=View.VISIBLE
+            binding!!.ivSearchCurrentLocation.visibility= View.VISIBLE
+            binding!!.etSearchSearchEdit.clearFocus()
+            binding!!.flSearch.requestFocus()
+            binding!!.searchBottomSheet.visibility= View.GONE
+            hideKeyboard()
+        }
+        else if(status ==2 ){
+            displayStatus=2
+            binding!!.clSearchWhite.visibility=View.VISIBLE
+            binding!!.searchMapview.visibility=View.INVISIBLE
+            binding!!.ivSearchCurrentLocation.visibility=View.INVISIBLE
+            binding!!.tvSearchRecent.visibility=View.VISIBLE
+            binding!!.ivSearchDefault.visibility=View.VISIBLE
+            binding!!.tvSearchDefault.visibility=View.VISIBLE
+            binding!!.searchBottomSheet.visibility= View.GONE
+        }
+        else if(status == 3){
+            displayStatus=3
+            binding!!.etSearchSearchEdit.clearFocus()
+            binding!!.flSearch.requestFocus()
+            setMarginBottom()
+            binding!!.tvSearchRecent.visibility=View.INVISIBLE
+            binding!!.ivSearchDefault.visibility=View.INVISIBLE
+            binding!!.tvSearchDefault.visibility=View.INVISIBLE
+            binding!!.clSearchWhite.visibility= View.INVISIBLE
+            binding!!.searchMapview.visibility=View.VISIBLE
+            binding!!.ivSearchCurrentLocation.visibility= View.VISIBLE
+            binding!!.searchBottomSheet.visibility= View.VISIBLE
+
+        }
+
+    }
+    private fun hideKeyboard(){
+//        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+        val activity = activity as MainActivity
+
+        val view = activity.currentFocus
+        if (view != null) {
+            val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view!!.getWindowToken(), 0)
+        }
+    }
+    private fun setMarginBottom(){
+        val params = binding!!.ivSearchCurrentLocation.layoutParams as ViewGroup.MarginLayoutParams
+        if(displayStatus==1){
+            params.setMargins(0,0,getPixel(requireContext(),25f).toInt(),getPixel(requireContext(),20f).toInt())
+        }
+        else{
+            params.setMargins(0,0,getPixel(requireContext(),25f).toInt(),getPixel(requireContext(),70f).toInt())
+        }
+
+        binding!!.ivSearchCurrentLocation.layoutParams = params
+    }
+    private fun getPixel(context: Context, dp : Float): Float{
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
+
     }
 
     // TODO Neither network gps ->  ?
     val permReqLuncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
         if (it) {
             mapViewContainer.addView(mapView)
-            for(i in 1..300){
+            for(i in 1..10){
                 addTestPOIItem(i)
+
             }
             initObserve()
+
         } else {
             // Failed pass
         }
-
     }
     private fun checkPermission(){
         permReqLuncher.launch(permission)
@@ -93,6 +208,41 @@ class PrayerRoomFragment : Fragment(),MapView.MapViewEventListener{
         mapView.addPOIItem(marker)
     }
 
+    private fun initBottomSheetView(){
+        var bottomsheet = BottomSheetBehavior.from(binding!!.searchBottomSheet)
+        bottomsheet.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback(){
+            override fun onStateChanged(bottomsheet: View, newState: Int) {
+                // BottomSheetBehavior의 5가지 상태
+                when(newState) {
+
+                    // 사용자가 BottomSheet를 위나 아래로 드래그 중인 상태
+                    BottomSheetBehavior.STATE_DRAGGING -> { }
+
+                    // 드래그 동작 후 BottomSheet가 특정 높이로 고정될 때의 상태
+                    // SETTLING 후 EXPANDED, SETTLING 후 COLLAPSED, SETTLING 후 HIDDEN
+                    BottomSheetBehavior.STATE_SETTLING -> { }
+
+                    // 최대 높이로 보이는 상태
+                    BottomSheetBehavior.STATE_EXPANDED -> { }
+
+                    // peek 높이 만큼 보이는 상태
+                    BottomSheetBehavior.STATE_COLLAPSED -> { }
+
+                    // 숨김 상태
+                    BottomSheetBehavior.STATE_HIDDEN -> { }
+
+                }
+            }
+
+            override fun onSlide(p0: View, slideOffset: Float) {
+                // slideOffset 범위: -1.0 ~ 1.0
+                // -1.0 HIDDEN, 0.0 COLLAPSED, 1.0 EXPANDED
+            }
+        })
+
+    }
+
+    // bottom nav height in activity
     override fun onMapViewInitialized(p0: MapView?) {
 
     }
--- Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/feature/main/prayerRoomFragment/PrayerRoomViewModel.kt
+++ Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/feature/main/prayerRoomFragment/PrayerRoomViewModel.kt
@@ -1,7 +1,40 @@
 package com.kosalaamInc.kosalaam.feature.main.prayerRoomFragment
 
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kosalaamInc.kosalaam.util.Event
 
 class PrayerRoomViewModel : ViewModel(){
+
+    private val _focus_et = MutableLiveData<Event<Boolean>>()
+    private val _back_bt1 = MutableLiveData<Event<Boolean>>()
+    private val _searchKey_bt = MutableLiveData<Event<Boolean>>()
+
+
+
+    val focus_et: LiveData<Event<Boolean>> get() = _focus_et
+    val back_bt : LiveData<Event<Boolean>> get() = _back_bt1
+    val searchKey_bt : LiveData<Event<Boolean>> get() = _searchKey_bt
+
+    fun onFocusEvent(){
+        _focus_et.value = Event(true)
+    }
+    fun onBack1Event(){
+        _back_bt1.value =Event(true)
+    }
+    fun searchKey(view: View, actionId: Int, event: KeyEvent?):Boolean{
+        if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+            _searchKey_bt.value=Event(true)
+            Log.d("searchKey","true")
+            return true
+        }
+        return false
+    }
+
 
 }
