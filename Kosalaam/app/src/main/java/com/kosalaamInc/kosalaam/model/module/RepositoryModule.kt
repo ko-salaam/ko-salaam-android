849b72257fb5282bb6b55f60da316006385b3797
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-02-15 17:00:03 +0900

    refactory : add repositoryModule

--- Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/model/module/RepositoryModule.kt
+++ Kosalaam/app/src/main/java/com/kosalaamInc/kosalaam/model/module/RepositoryModule.kt
@@ -2,7 +2,9 @@ package com.kosalaamInc.kosalaam.model.module
 
 import com.kosalaamInc.kosalaam.model.network.api.KosalaamAPI
 import com.kosalaamInc.kosalaam.repository.LikeRepository
+import com.kosalaamInc.kosalaam.repository.PrayerRoomHostRepository
 import com.kosalaamInc.kosalaam.repository.SearchRepository
+import com.kosalaamInc.kosalaam.repository.UserRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,4 +22,12 @@ object RepositoryModule{
     @Singleton
     @Provides
     fun provideLikeRepository(apiService:KosalaamAPI)= LikeRepository(apiService)
+
+    @Singleton
+    @Provides
+    fun provideUserRespository(apiService: KosalaamAPI) = UserRepository(apiService)
+
+    @Singleton
+    @Provides
+    fun providePrayerRoomHostRepository(apiService : KosalaamAPI) = PrayerRoomHostRepository(apiService)
 }
